5f240d616c91ac0c121df310ec041a74a6edb894
David Neumann <david.neumann@lecode.de>
2017-01-12 15:03:06 +0100

    Initial commit of the project.

---
+++ README.md
@@ -0,0 +1,2 @@
+# TypeScript Globalization Binding Handlers
+Knockout binding handlers for date and numerical objects of the globalization library written in TypeScript. It supports binding dates and numerics to text inputs based on different cultures.
---
+++ src/Typings/References.d.ts
@@ -0,0 +1,4 @@
+
+/// <reference path="../../bower_components/DefinitelyTyped/jquery/index.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/knockout/index.d.ts" />
+/// <reference path="../../bower_components/typescript-globalization/build/typescript-globalization.d.ts" />
